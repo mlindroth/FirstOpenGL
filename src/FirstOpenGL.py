f804a5636fe9b92f7b23d2b036c2e27e7b2bfb89
Michael C. Lindroth <michael.lindroth@googlemail.com>
2022-09-20 14:40:16 +0200

    changed from grid to cars

--- src/FirstOpenGL.py
+++ src/FirstOpenGL.py
@@ -1,5 +1,8 @@
 import os
-import numpy as np
+import struct
+
+from colorsys import hls_to_rgb as hls
+from random import uniform
 
 import moderngl as mgl
 import moderngl_window as mglw
@@ -7,11 +10,18 @@ import moderngl_window as mglw
 from Camera import Camera
 
 
-def grid(size, steps):
-    u = np.repeat(np.linspace(-size, size, steps), 2)
-    v = np.tile([-size, size], steps)
-    w = np.zeros(steps * 2)
-    return np.concatenate([np.dstack([u, v, w]), np.dstack([v, u, w])])
+def random_color():
+    return hls(uniform(0.0, 1.0), 0.5, 0.5)
+
+
+cars = []
+cars += [{'color': random_color(),
+          'pos': (i * 2.0 - 9.0, 0.0, 1.5),
+          'angle': uniform(-0.5, 0.5)} for i in range(10)]
+
+cars += [{'color': random_color(),
+          'pos': (i * 2.0 - 9.0, 0.0, -1.5),
+          'angle': uniform(-0.5, 0.5)} for i in range(10)]
 
 
 class PerspectiveProjection(mglw.WindowConfig):
@@ -28,31 +38,64 @@ class PerspectiveProjection(mglw.WindowConfig):
 
         self.prog = self.ctx.program(
             vertex_shader='''
-                #version 330
+                            #version 330
 
-                uniform mat4 Mvp;
+                            uniform mat4 Mvp;
 
-                in vec3 in_vert;
+                            in vec3 in_position;
+                            in vec3 in_normal;
 
-                void main() {
-                    gl_Position = Mvp * vec4(in_vert, 1.0);
-                }
-            ''',
+                            in vec3 in_color;
+                            in vec3 in_origin;
+                            in mat3 in_basis;
+
+                            out vec3 v_vert;
+                            out vec3 v_norm;
+                            out vec3 v_color;
+
+                            void main() {
+                                v_vert = in_origin + in_basis * in_position;
+                                v_norm = in_basis * in_normal;
+                                v_color = in_color;
+                                gl_Position = Mvp * vec4(v_vert, 1.0);
+                            }
+                        ''',
             fragment_shader='''
-                #version 330
+                            #version 330
 
-                out vec4 f_color;
+                            uniform vec3 Light;
+                            uniform sampler2D Texture;
 
-                void main() {
-                    f_color = vec4(0.1, 0.1, 0.1, 1.0);
-                }
-            ''',
+                            in vec3 v_vert;
+                            in vec3 v_norm;
+                            in vec3 v_color;
+
+                            out vec4 f_color;
+
+                            void main() {
+                                float lum = clamp(dot(normalize(Light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.8 + 0.2;
+                                f_color = vec4(v_color * lum, 1.0);
+                            }
+                        ''',
         )
 
         self.camera = Camera(self.aspect_ratio)
         self.mvp = self.prog['Mvp']
-        self.vbo = self.ctx.buffer(grid(15, 10).astype('f4'))
-        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert')
+        self.light = self.prog['Light']
+
+        obj = self.load_scene('lowpoly_toy_car.obj')
+
+        self.vbo = self.ctx.buffer(struct.pack(
+            '15f',
+            1.0, 1.0, 1.0,
+            0.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0,
+        ) * len(cars))
+        vao_wrapper = obj.root_nodes[0].mesh.vao
+        vao_wrapper.buffer(self.vbo, '3f 3f 9f/i', ['in_color', 'in_origin', 'in_basis'])
+        self.vao = vao_wrapper.instance(self.prog)
 
         self.states = {
             self.wnd.keys.W: False,     # forward
@@ -115,7 +158,18 @@ class PerspectiveProjection(mglw.WindowConfig):
         self.ctx.enable(mgl.DEPTH_TEST)
 
         self.mvp.write((self.camera.mat_projection * self.camera.mat_lookat).astype('f4'))
-        self.vao.render(mgl.LINES)
+
+        self.vbo.write(b''.join(struct.pack(
+            '15f',
+            *car['color'],
+            *car['pos'],
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0,
+        ) for car in cars))
+        self.vao.render(instances=len(cars))
+
+        self.vao.render(instances=len(cars))
 
 
 if __name__ == '__main__':
